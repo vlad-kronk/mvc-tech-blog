ebaae9c00517c6630d4efe09575be0f134041fee
Jacob Meyers <jacobmeyers6@outlook.com>
2023-01-09 16:43:43 -0800

    added seed json files

--- models/Post.js
+++ models/Post.js
@@ -1,5 +1,4 @@
 const { Model, DataTypes } = require('sequelize');
-const { default: ModelManager } = require('sequelize/types/model-manager');
 const sequelize = require('../config/connection');
 
 class Post extends Model { }
