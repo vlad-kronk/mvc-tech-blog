c3df03b4f5256894608633c48ca5dc2cd76418b6
Jacob Meyers <jacobmeyers6@outlook.com>
2023-01-30 13:42:09 -0800

    added login/logout/signup routes, login handlebars page, and custom overlayed scrollbar

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,69 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// log in
+router.post('/login', async (req, res) => {
+   try {
+      // look for the user with the given username in db
+      const dbUserData = await User.findOne({
+         where: {
+            username: req.body.username
+         }
+      });
+      // if db did not contain a user with the given username
+      if (!dbUserData) {
+         res.status(400).json({ message: 'Incorrect username.' });
+         return;
+      }
+      // call user instance method to check password using bcrypt
+      const passwordIsValid = await dbUserData.checkPassword(req.body.password);
+      // if check password method returned false
+      if (!passwordIsValid) {
+         res.status(400).json({ message: 'Incorrect password.' });
+         return;
+      }
+      // if code makes it to this point, user has successfully logged in
+      req.session.save(() => {
+         req.session.logged_in = true; // used by browser to mark user as logged in
+         req.session.user_id = dbUserData.id;
+         res.status(200).json({ user: dbUserData, message: "You are now logged in." });
+      });
+   } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+   }
+});
+
+// log out
+router.post('/logout', async (req, res) => {
+   // if the user is logged in
+   if (req.session.loggedIn) {
+      // destroy the session
+      req.session.destroy(() => {
+         res.status(204).end();
+      });
+   } else {
+      res.status(404).end();
+   }
+})
+
+// sign up
+router.post('/signup', async (req, res) => {
+   try {
+      // create a user with the info provided in the request body
+      const dbUserData = await User.create({
+         name: req.body.name,
+         username: req.body.username,
+         password: req.body.password
+      });
+      // log in as the newly created user and save the session
+      req.session.save(() => {
+         req.session.logged_in = true; // used by browser to mark user as logged in
+         req.session.user_id = dbUserData.id;
+         res.status(200).json({ user: dbUserData, message: "Signed up and logged in." });
+      });
+   } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+   }
+});
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
+const e = require("express");
 const moment = require("moment")
 
 // import models
 const { Post, User, Comment } = require('../models');
 
-// homepage
+// homepage home route
 router.get('/', async (req, res) => {
    try {
       // gets all posts including authors and dates
@@ -20,13 +21,12 @@ router.get('/', async (req, res) => {
             }
          ]
       })
-
+      // creates an array of post previews, formatted for display
       const post_previews = dbPosts.map((post) => {
-
          // only need date part of timestamp
          let date = JSON.stringify(post.updatedAt);
          date = date.substring(1, 11);
-
+         // formats object for returned array
          const result = {
             id: post.id,
             title: post.title,
@@ -34,26 +34,32 @@ router.get('/', async (req, res) => {
             // pre-formats the date for display
             updated_at: moment(date, 'YYYY-MM-DD').format('MMM DD, YYYY')
          };
-
          return result;
       })
-
-      // res.status(200).json(homepage_info);
-
+      // array of all required page data
       let res_data = {
          page_title: 'Home',
-         // logged_in: req.session.logged_in,
-         logged_in: true,
+         logged_in: req.session.logged_in,
+         // logged_in: true,
          posts: post_previews
       }
-
-      // res.status(200).json({ data: res_data });
-
+      // render the page using the formatted data
       res.render('home', { data: res_data });
    } catch (err) {
       console.log(err);
       res.status(500).json(err);
    }
 });
+
+// login home route
+router.get('/login', async (req, res) => {
+   // if user is already logged in, redirect to the homepage
+   if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+   }
+   // if not, render login page
+   res.render('login');
+})
 
 module.exports = router;
