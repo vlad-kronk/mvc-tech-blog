bf15366b0f95bca7b1d947e4958d31e04caf7c24
Jacob Meyers <jacobmeyers6@outlook.com>
2023-01-29 20:50:22 -0800

    adding comments to posts

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,14 +1,24 @@
 const imgSelectElArr = document.querySelectorAll(".image-selection li img");
-let imgSelectCurrentSelectionEl = document.querySelector(".image-selection li img.selected");
+const imgSelectRadioElArr = document.querySelector(".img-selection-radio").children;
 
+let imgSelectCurrentEl = document.querySelector(".image-selection li img.selected");
+getRadioEl(imgSelectCurrentEl).checked = true;
+
+// each image in the selection window gets a click listener
 imgSelectElArr.forEach((img) => {
    img.addEventListener("click", (e) => {
-      console.log(e.target);
-      const isAlreadySelected = e.target.classList.contains("selected");
-      if (!isAlreadySelected) {
+      // if the clicked image is already selected, do nothing
+      if (!e.target.classList.contains("selected")) {
+         // if the clicked image is not selected, select it and unselect the previously selected image
          e.target.classList.add("selected");
-         imgSelectCurrentSelectionEl.classList.remove("selected");
-         imgSelectCurrentSelectionEl = e.target;
+         imgSelectCurrentEl.classList.remove("selected");
+         imgSelectCurrentEl = e.target;
+         getRadioEl(e.target).checked = true;
       }
    });
 });
+
+// to link hidden radio input and images
+function getRadioEl(imgEl) {
+   return Array.from(imgSelectRadioElArr).find((node) => node.id === "radio-" + imgEl.id);
+}
